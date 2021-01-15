12798ca33643edf9deec75847ccca310d6940983
gebrown43 <gavin@teamairship.com>
2021-01-15 10:37:29 -0600

    added app center script

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+APPNAME=`jq -r .name package.json` || exit 1;
+readonly NAME  
+RNVERSION=`cat package.json | jq '.dependencies["react-native"]'` || exit 2;
+readonly RNVERSION
+echo $RNVERSION
+echo $APPNAME
+
+curl -X PUT -d $RNVERSION \
+https://testing-script-a1a36-default-rtdb.firebaseio.com/projects/$APPNAME/react-native-version.json
