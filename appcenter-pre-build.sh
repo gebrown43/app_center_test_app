ee8483b537196b47b720830f0416957cf2420378
gebrown43 <gavin@teamairship.com>
2021-02-04 10:46:44 -0600

    lets see if it will run in gitignore

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -4,8 +4,10 @@ APPNAME=`jq -r .name package.json` || exit 1;
 readonly APPNAME  
 RNVERSION=`cat package.json | jq '.dependencies["react-native"]'` || exit 2;
 readonly RNVERSION
+ACCESSTOKEN=$(node ./authenticationScript.js)
+readonly ACCESSTOKEN
 echo $RNVERSION
 echo $APPNAME
 
 curl -X PUT -d $RNVERSION \
-https://version-tracker-app-default-rtdb.firebaseio.com/projects/$APPNAME/react-native-version.json
+https://version-tracker-app-default-rtdb.firebaseio.com/projects/$APPNAME/react-native-version.json?access_token=$ACCESSTOKEN
