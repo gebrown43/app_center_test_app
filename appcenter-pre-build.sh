0b396d9ce935d0725777858b4c965eaf25a31f07
gebrown43 <gavin@teamairship.com>
2021-02-09 16:53:47 -0600

    added node script instead

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
+node ./authenticationScript.js
 
-APPNAME=`jq -r .name package.json` || exit 1;
-readonly APPNAME  
-RNVERSION=`cat package.json | jq '.dependencies["react-native"]'` || exit 2;
-readonly RNVERSION
-ACCESSTOKEN=$(node ./authenticationScript.js) || exit 3
-readonly ACCESSTOKEN
-echo $RNVERSION
-echo $APPNAME
 
-curl -X PUT -d $RNVERSION \
-https://version-tracker-app-default-rtdb.firebaseio.com/projects/$APPNAME/react-native-version.json?access_token=$ACCESSTOKEN
+  
--- authenticationScript.js
+++ authenticationScript.js
@@ -1,27 +1,63 @@
-var {google} = require('googleapis');
-var serviceAccount = require('./versionTrackerKey.json');
-
-var scopes = [
-  'https://www.googleapis.com/auth/userinfo.email',
-  'https://www.googleapis.com/auth/firebase.database',
-];
-
-var jwtClient = new google.auth.JWT(
-  serviceAccount.client_email,
-  null,
-  serviceAccount.private_key,
-  scopes,
-);
-
-jwtClient.authorize((err, tokens) => {
-  if (err) {
-    console.log('Error making request to generate access token:', err);
-  } else if (tokens.access_token === null) {
-    console.log(
-      'Provided service account does not have permission to generate access tokens',
-    );
-  } else {
-    var accessToken = tokens.access_token;
-    console.log(accessToken);
+const fetch = require('node-fetch');
+const mutation = `
+mutation {
+  signIn(input: { email: "api@teamairship.com", password: "Airship123" }) {          
+    success
+    user {
+        authenticationToken
+    }          
   }
-});
+}
+`;
+
+const url = 'https://crew-api-staging.herokuapp.com/graphql';
+
+fetch(url, {
+  method: 'POST',
+  headers: {'Content-Type': 'application/json'},
+  body: JSON.stringify({query: mutation}),
+})
+  .then((res) => res.json())
+  .then((data) => {
+    if (data.data) {
+      const {authenticationToken} = data.data.signIn.user;
+      saveApp(authenticationToken);
+    }
+  })
+  .catch((err) => console.log('Err in authentication', err));
+
+function saveApp(authenticationToken) {
+  const packageJSON = require('./package.json');
+  const body = `
+  mutation saveApp($name: String!, $version: String!, $platform: AppPlatform!) {
+    saveApp(input: { name: $name, version: $version, platform: $platform }) {
+      app {
+        id
+        name
+        platform
+        version
+      }
+      success
+      errors {
+          message
+          path
+      }        
+    }
+  }
+  `;
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${authenticationToken}`,
+    },
+    body: JSON.stringify({
+      query: body,
+      variables: {
+        name: `${packageJSON.name}`,
+        version: `${packageJSON.dependencies['react-native']}`,
+        platform: 'REACT_NATIVE',
+      },
+    }),
+  }).catch((err) => console.log('ERR', err));
+}
